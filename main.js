2bd16798200edd2ce8a554caa963f43cbcdbf880
Tony Cain <r.tonycain@gmail.com>
2020-06-16 20:42:43 -0400

    piecsing together

---
+++ main.js
@@ -0,0 +1,19 @@
+let mainElement = document.createElement('main')
+mainElement.append('body')
+
+let scriptElement = document.createElement('script')
+scriptElement.append('body')
+
+let buttonElement = document.createElement('button')
+buttonElement.append('body')
+//buttonElement.addEventListener('click',); 
+
+let imgElement = document.createElement('img')
+imgElement.src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTpMu9mbECH3CVRqvUfst1jjskmEpw5cTikX8hvXSwSKFU3Edc7&usqp=CAU'
+imgElement.append('main')
+
+let aLinkElement = document.createElement('a')
+aLinkElement.append('main')
+
+console.log ('hello')
+console.log(imgElement)
